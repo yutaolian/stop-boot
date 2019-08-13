8835799820ccadf429fab80ca0a50edf84428233
lianyutao <lianyutao@gaosiedu.com>
2019-08-13 21:23:46 +0800

    add vue demo

--- stop-boot-admin-vue/src/components/HelloWorld.vue
+++ stop-boot-admin-vue/src/components/HelloWorld.vue
@@ -1,40 +1,35 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <h1>{{ msg }} </h1>
+    <el-form :model="loginForm">
+      <h2>Ray商城登录</h2>
+      <el-form-item>
+        <el-input name="username" placeholder="请输入用户名/手机号" auto-complete="on"></el-input>
+      </el-form-item>
+
+      <el-form-item>
+        <el-input  name="password" placeholder="请输入密码" auto-complete="on"></el-input>
+      </el-form-item>
+
+      <el-form-item>
+        <el-button type="primary">登录</el-button>
+      </el-form-item>
+    </el-form>
   </div>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  data(){
+    return{
+      msg:"this is a vue demo"
+    }
+  },
+  mounted() {
+    this.$axios.get("http://localhost:8080/").then(res=>{
+      alert(res.data)
+    });
   }
 }
 </script>
--- stop-boot-admin-vue/src/main.js
+++ stop-boot-admin-vue/src/main.js
@@ -1,7 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import axios from "axios"
 
+
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+Vue.use(ElementUI)
 Vue.config.productionTip = false
+Vue.prototype.$axios = axios;
+
 
 new Vue({
   render: h => h(App),
